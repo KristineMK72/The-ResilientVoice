664466933ee8c74bf1d8508f59598c6dd254250d
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-15 14:17:17 -0600

    Create Home component with welcome message and product grid

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import ProductGrid from "../components/ProductGrid";
+
+
+export default function Home() {
+return (
+<main>
+<h2>Welcome to Resilient Voice</h2>
+<p>Faith-inspired apparel and goods with purpose.</p>
+<ProductGrid />
+</main>
+);
+}
