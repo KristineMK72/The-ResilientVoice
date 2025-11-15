a3d26acd0443978378ddbfa0b73aa7356484cd45
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-15 14:17:47 -0600

    Create accessories.js

---
+++ pages/accessories.js
@@ -0,0 +1,12 @@
+import ProductGrid from "../components/ProductGrid";
+
+
+export default function Accessories() {
+return (
+<main>
+<h2>Accessories & More</h2>
+<p>Hats, totes, mugs, and accessories designed with purpose.</p>
+<ProductGrid category="accessories" />
+</main>
+);
+}
