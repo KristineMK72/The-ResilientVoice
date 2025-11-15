cfd99cca759d5be05330a5e530f125d66a858d51
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-15 14:18:40 -0600

    Create grace.js

---
+++ pages/grace.js
@@ -0,0 +1,12 @@
+import ProductGrid from "../components/ProductGrid";
+
+
+export default function Grace() {
+return (
+<main>
+<h2>Grace Collection</h2>
+<p>Soft designs and messages of faith and healing.</p>
+<ProductGrid category="grace" />
+</main>
+);
+}
