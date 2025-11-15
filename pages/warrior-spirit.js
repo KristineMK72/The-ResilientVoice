8e5f2442ca0339fa5acfa504aff58b33cc2ed1e4
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-15 14:19:04 -0600

    Create warrior-spirit.js

---
+++ pages/warrior-spirit.js
@@ -0,0 +1,12 @@
+import ProductGrid from "../components/ProductGrid";
+
+
+export default function WarriorSpirit() {
+return (
+<main>
+<h2>Warrior Spirit Collection</h2>
+<p>Stand bold in your faith.</p>
+<ProductGrid category="warrior" />
+</main>
+);
+}
