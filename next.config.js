905591c6e099bc3a6caf25edcf2c8f488d108dc2
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-15 14:37:58 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-reactStrictMode: true,
-images: {
-domains: ["files.printful.com", "printful.com"],
-},
+  reactStrictMode: true,
+  images: {
+    domains: ["files.printful.com", "printful.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.printful.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 module.exports = nextConfig;
