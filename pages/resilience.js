1e3abaf5cd6485823cfb0d7d3902171883019270
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-15 14:18:13 -0600

    Create resilience.js

---
+++ pages/resilience.js
@@ -0,0 +1,12 @@
+import ProductGrid from "../components/ProductGrid";
+
+
+export default function Resilience() {
+return (
+<main>
+<h2>Resilience Collection</h2>
+<p>Wear messages of strength and endurance.</p>
+<ProductGrid category="resilience" />
+</main>
+);
+}
