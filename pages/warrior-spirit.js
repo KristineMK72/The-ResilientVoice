fd2fd99b39e23daab29ac85404d45e65de3c609c
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-16 11:37:50 -0600

    Add classes to Warrior Spirit component elements

--- pages/warrior-spirit.js
+++ pages/warrior-spirit.js
@@ -1,12 +1,11 @@
 import ProductGrid from "../components/ProductGrid";
 
-
 export default function WarriorSpirit() {
-return (
-<main>
-<h2>Warrior Spirit Collection</h2>
-<p>Stand bold in your faith.</p>
-<ProductGrid category="warrior" />
-</main>
-);
+  return (
+    <main>
+      <h2 className="shop-title">Warrior Spirit Collection</h2>
+      <p className="shop-description">Stand bold in your faith.</p>
+      <ProductGrid category="warrior" />
+    </main>
+  );
 }
