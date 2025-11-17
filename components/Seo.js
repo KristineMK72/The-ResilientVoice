bc9491b9d8234fbd86181e621a5c628c2984f995
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-16 18:23:10 -0600

    Create Seo.js

---
+++ components/Seo.js
@@ -0,0 +1,11 @@
+// components/Seo.js
+import Head from 'next/head'
+
+export default function Seo({ title, description }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+    </Head>
+  )
+}
