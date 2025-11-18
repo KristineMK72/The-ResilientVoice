09bc5447ec63c57760de6751f6e1902b5da4f031
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-18 12:16:22 -0600

    Update warrior-spirit.js

--- pages/warrior-spirit.js
+++ pages/warrior-spirit.js
@@ -4,7 +4,7 @@ import ProductGrid from "../components/ProductGrid";
 export default function Warrior() {
 return (
 <main>
-<h2>Accessories & More</h2>
+<h2>Warrior Spirit Co. </h2>
 <p>For the Warriors.</p>
 <ProductGrid category="warrior" />
 </main>
