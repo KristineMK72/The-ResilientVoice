ccc3abee8f6c571bdca640e82e00004aff5453aa
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-29 09:59:28 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,16 @@
 import Link from "next/link";
 
-
 export default function Header() {
-return (
-<header className="header">
-<h1>Resilient Voice</h1>
-<nav>
-<Link href="/">Home</Link>
-<Link href="/gallery">Gallery</Link>
-<Link href="/accessories">Accessories</Link>
-<Link href="/resilience">Resilience</Link>
-<Link href="/grace">Grace</Link>
-<Link href="/warrior-spirit">Warrior Spirit</Link>
-<Link href="/blog">Blog</Link>
-<Link href="/cart">Cart</Link>
-</nav>
-</header>
-);
+  return (
+    <header className="header">
+      <h1>Resilient Voice</h1>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/saved-by-grace">Saved By Grace</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/about">About</Link>
+        <Link href="/cart">Cart</Link>
+      </nav>
+    </header>
+  );
 }
