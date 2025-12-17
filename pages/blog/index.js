8ba9b3a9d13ef247e985dd3513a36d309fda96a3
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-12-16 23:37:08 -0600

    Update index.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -12,7 +12,9 @@ export default function BlogIndex() {
       <main style={{ backgroundColor: "#f9f5f1", minHeight: "100vh", padding: "40px 20px" }}>
         <section style={{ maxWidth: 800, margin: "0 auto" }}>
           <h1 style={{ marginTop: 0 }}>Blog</h1>
-
+          <li style={{ marginBottom: 10 }}>
+          <Link href="/blog/christmas">Christmas</Link>
+          </li>
           <ul style={{ paddingLeft: 18, marginTop: 24 }}>
             <li style={{ marginBottom: 10 }}>
               <Link href="/blog/storms">Storms</Link>
