74a26a7c20f54dd8b813f2bb924fa4e3076b8467
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-03-16 22:55:08 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import GlobalStoreAssistant from "../components/GlobalStoreAssistant";
 import "../styles/global.css";
+import "leaflet/dist/leaflet.css";
 
 function MyApp({ Component, pageProps }) {
   return (
